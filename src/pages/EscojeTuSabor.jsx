192b17800918634dc5a56a2f6eb9d6c8ab254476
Steven Borbor <steven.borbor@gmail.com>
2023-06-19 11:25:49 -0500

    create text with number and style

--- src/pages/EscojeTuSabor.jsx
+++ src/pages/EscojeTuSabor.jsx
@@ -12,6 +12,21 @@ const EscojeTuSabor = () => {
   const [tipo, setTipo] = useState("");
   const [producto, setProducto] = useState("");
 
+  const selectColor = (producto) => {
+    let color;
+    if (producto == "BebidadeAlmendras") {
+      color = "#5C7C38";
+    }
+    if (producto == "LecheBlanca") {
+      color = "#001D85";
+    }
+    if (producto == "Griego") {
+      color = "#80B7FF";
+    }
+
+    return color;
+  };
+
   useEffect(() => {
     setTipo(location.state.tipo);
     setProducto(location.state.producto);
@@ -20,17 +35,24 @@ const EscojeTuSabor = () => {
   return (
     <div>
       <div className={`contenedor_Escojetusabor ${producto}`}>
-        <CabezeraInterfaz producto={producto}/>
+        <CabezeraInterfaz producto={producto} />
         <div className="contenedor_texto">
-            <div className="numero_titulo">
-                <NumeroDos/>
-                <p>Escoge tu sabor preferido</p>
-            </div>
-            <div className="numero_subtitulo">
-                
-            </div>
+          <motion.div
+            className="numero_titulo"
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ back: "backIn", duration: 0.5, delay: 0.2 }}
+          >
+            <NumeroDos color={selectColor(producto)} />
+            <p style={{ color: selectColor(producto) }}>
+              Escoge tu sabor preferido
+            </p>
+          </motion.div>
+          <div className="numero_subtitulo">
+            <p>Escoge solo 1</p>
+          </div>
         </div>
-        <BotonAtras color={producto=="LecheBlanca"?"white":"#757677"}/>
+        <BotonAtras color={producto == "LecheBlanca" ? "white" : "#757677"} />
       </div>
     </div>
   );
